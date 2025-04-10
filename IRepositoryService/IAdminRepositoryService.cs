50fdc677b65ee97de91401fe4aa5cc396f3b7d62
MahmoudHossam00 <mahmodmohammed345@gmail.com>
2025-04-10 15:21:45 +0200

    AdminRepoService

---
+++ IRepositoryService/IAdminRepositoryService.cs
@@ -0,0 +1,12 @@
+﻿namespace Freelancing.IRepositoryService
+{
+	public interface IAdminRepositoryService
+	{
+		Task<bool> AddAdmin(Admin Admin);
+		Task<bool> UpdateAdmin(Admin Admin);
+		Task<bool> DeleteAdmin(string id);
+		Task<Admin> GetAdminById(string id);
+		Task<IEnumerable<Admin>> GetAllAdmins();
+		Task<bool> AdminExists(string id);
+	}
+}
---
+++ RepositoryService/AdminRepositoryService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Freelancing.RepositoryService
+{
+	public class AdminRepositoryService(ApplicationDbContext _context):IAdminRepositoryService
+	{
+		public async Task<bool> AddAdmin(Admin Admin)
+		{
+			await _context.Admins.AddAsync(Admin);
+			return await _context.SaveChangesAsync() > 0;
+		}
+
+		public async Task<bool> AdminExists(string id)
+		{
+			return await _context.Admins.AnyAsync(c => c.Id == id);
+		}
+
+		public async Task<bool> DeleteAdmin(string id)
+		{
+			var Admin = await _context.Admins.FirstOrDefaultAsync(c => c.Id == id);
+			if (Admin == null)
+			{
+				return false;
+			}
+			Admin.isDeleted = true;
+			_context.Admins.Update(Admin);
+			return await _context.SaveChangesAsync() > 0;
+		}
+
+		public async Task<IEnumerable<Admin>> GetAllAdmins()
+		{
+			return await _context.Admins.ToListAsync();
+		}
+
+		public async Task<Admin> GetAdminById(string id)
+		{
+			return await _context.Admins.FirstOrDefaultAsync(c => c.Id == id);
+		}
+
+		public async Task<bool> UpdateAdmin(Admin Admin)
+		{
+			var admin = await GetAdminById(Admin.Id);
+			if (admin == null)
+			{
+				return false;
+			}
+			else
+			{
+				_context.Admins.Update(Admin);
+				return await _context.SaveChangesAsync() > 0;
+			}
+
+		}
+	}
+}
