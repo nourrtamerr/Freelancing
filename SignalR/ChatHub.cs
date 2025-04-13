17017f2cbe2401b4601ba175c99836cafc4c20ac
AliIsmail10 <ali.mohamed.ali31199@gmail.com>
2025-04-13 03:36:02 +0200

    implement chat controller with SignalR

--- SignalR/ChatHub.cs
+++ SignalR/ChatHub.cs
@@ -11,13 +11,13 @@ namespace Freelancing.SignalR
                 await Clients.Users(chatDto.SenderId,chatDto.ReceiverId).SendAsync("ReceiveMessage",chatDto);
         }
 
-        public async override Task OnConnectedAsync()
-        {
+        //public async override Task OnConnectedAsync()
+        //{
 
-            var userId = Context.UserIdentifier;
-            await Groups.AddToGroupAsync(Context.ConnectionId, userId);
-            await base.OnConnectedAsync();
-        }
+        //    var userId = Context.UserIdentifier;
+        //    await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+        //    await base.OnConnectedAsync();
+        //}
     }
     
 }
