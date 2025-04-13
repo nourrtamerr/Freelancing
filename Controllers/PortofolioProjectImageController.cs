464d0a2dc02184bc25b26c8edf5c621f5af4fe66
Malak Emad <malakemad799@gmail.com>
2025-04-14 01:29:16 +0200

    ay haga

--- Controllers/PortofolioProjectImageController.cs
+++ Controllers/PortofolioProjectImageController.cs
@@ -7,32 +7,19 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Freelancing.DTOs;
 using Freelancing.Models;
+using AutoMapper;
 
 namespace Freelancing.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PortofolioProjectImageController : ControllerBase
+    public class PortofolioProjectImageController(IPortofolioProjectImage context, IMapper mapper) : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
-
-        public PortofolioProjectImageController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/PortofolioProjectImage
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<PortofolioProjectImageDTO>>> GetPortofolioProjectImageDTO()
-        {
-            return await _context.PortofolioProjectImageDTO.ToListAsync();
-        }
-
         // GET: api/PortofolioProjectImage/5
         [HttpGet("{id}")]
         public async Task<ActionResult<PortofolioProjectImageDTO>> GetPortofolioProjectImageDTO(int id)
         {
-            var portofolioProjectImageDTO = await _context.PortofolioProjectImageDTO.FindAsync(id);
+            var portofolioProjectImageDTO = await context.GetByPortfolioProjectIdAsync(id);
 
             if (portofolioProjectImageDTO == null)
             {
@@ -40,37 +27,6 @@ namespace Freelancing.Controllers
             }
 
             return portofolioProjectImageDTO;
-        }
-
-        // PUT: api/PortofolioProjectImage/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPortofolioProjectImageDTO(int id, PortofolioProjectImageDTO portofolioProjectImageDTO)
-        {
-            if (id != portofolioProjectImageDTO.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(portofolioProjectImageDTO).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PortofolioProjectImageDTOExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
         }
 
         // POST: api/PortofolioProjectImage
@@ -100,9 +56,5 @@ namespace Freelancing.Controllers
             return NoContent();
         }
 
-        private bool PortofolioProjectImageDTOExists(int id)
-        {
-            return _context.PortofolioProjectImageDTO.Any(e => e.Id == id);
-        }
     }
 }
