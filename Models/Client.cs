acff24b6b8ccb765a8be5fb15aebc08f31a4fe02
MahmoudHossam00 <mahmodmohammed345@gmail.com>
2025-04-13 03:52:20 +0200

    models

--- Models/Client.cs
+++ Models/Client.cs
@@ -2,17 +2,51 @@
 
 namespace Freelancing.Models
 {
-	public class Client:AppUser
+	public class Client : AppUser
 	{
 		public bool PaymentVerified { get; set; } = false;
 
 
-        [ForeignKey("subscriptionPlan")]
-        public int? subscriptionPlanId { get; set; } = 2;
-        public virtual SubscriptionPlan? subscriptionPlan { get; set; }
+		[ForeignKey("subscriptionPlan")]
+		public int? subscriptionPlanId { get; set; } = 2;
+		public virtual SubscriptionPlan? subscriptionPlan { get; set; }
 
 
-        public int? RemainingNumberOfProjects { get; set; } = 6;
+		public int? RemainingNumberOfProjects { get; set; } = 6;
 
-    }
+		[NotMapped]
+		public Rank Rank
+		{
+			get
+			{
+				int count = Reviews.Count;
+				if (count == 0)
+					return Rank.Veteran;
+
+				double average = Reviews.Average(r => r.Rating);
+
+				if (count < 5 && average >= 3.5)
+					return Rank.RisingStar;
+				else if (count < 15 && average >= 4)
+					return Rank.Established;
+				else if (count < 30 && average >= 4.2)
+					return Rank.Pro;
+				else if (count >= 30 && average >= 4.5)
+					return Rank.Elite;
+
+				return Rank.RisingStar;
+			}
+		}
+
+	}
+    public enum Rank
+    {
+	
+		Veteran,       
+		RisingStar,    
+		Established,   
+		Pro,           
+		Elite          
+	
+	}
 }
