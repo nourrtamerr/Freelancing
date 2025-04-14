8297af901daf5dd5c50f11d0d04741bee0aef98f
Malak Emad <malakemad799@gmail.com>
2025-04-14 17:13:05 +0200

    m

--- Controllers/PortofolioProjectImageController.cs
+++ Controllers/PortofolioProjectImageController.cs
@@ -16,45 +16,45 @@ namespace Freelancing.Controllers
     public class PortofolioProjectImageController(IPortofolioProjectImage context, IMapper mapper) : ControllerBase
     {
         // GET: api/PortofolioProjectImage/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<PortofolioProjectImageDTO>> GetPortofolioProjectImageDTO(int id)
-        {
-            var portofolioProjectImageDTO = await context.GetByPortfolioProjectIdAsync(id);
+    //    [HttpGet("{id}")]
+    //    public async Task<ActionResult<PortofolioProjectImageDTO>> GetPortofolioProjectImageDTO(int id)
+    //    {
+    //        var portofolioProjectImageDTO = await context.GetByPortfolioProjectIdAsync(id);
 
-            if (portofolioProjectImageDTO == null)
-            {
-                return NotFound();
-            }
+    //        if (portofolioProjectImageDTO == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            return portofolioProjectImageDTO;
-        }
+    //        return portofolioProjectImageDTO;
+    //    }
 
-        // POST: api/PortofolioProjectImage
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<PortofolioProjectImageDTO>> PostPortofolioProjectImageDTO(PortofolioProjectImageDTO portofolioProjectImageDTO)
-        {
-            _context.PortofolioProjectImageDTO.Add(portofolioProjectImageDTO);
-            await _context.SaveChangesAsync();
+    //    // POST: api/PortofolioProjectImage
+    //    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    //    [HttpPost]
+    //    public async Task<ActionResult<PortofolioProjectImageDTO>> PostPortofolioProjectImageDTO(PortofolioProjectImageDTO portofolioProjectImageDTO)
+    //    {
+    //        _context.PortofolioProjectImageDTO.Add(portofolioProjectImageDTO);
+    //        await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetPortofolioProjectImageDTO", new { id = portofolioProjectImageDTO.Id }, portofolioProjectImageDTO);
-        }
+    //        return CreatedAtAction("GetPortofolioProjectImageDTO", new { id = portofolioProjectImageDTO.Id }, portofolioProjectImageDTO);
+    //    }
 
-        // DELETE: api/PortofolioProjectImage/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePortofolioProjectImageDTO(int id)
-        {
-            var portofolioProjectImageDTO = await _context.PortofolioProjectImageDTO.FindAsync(id);
-            if (portofolioProjectImageDTO == null)
-            {
-                return NotFound();
-            }
+    //    // DELETE: api/PortofolioProjectImage/5
+    //    [HttpDelete("{id}")]
+    //    public async Task<IActionResult> DeletePortofolioProjectImageDTO(int id)
+    //    {
+    //        var portofolioProjectImageDTO = await _context.PortofolioProjectImageDTO.FindAsync(id);
+    //        if (portofolioProjectImageDTO == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            _context.PortofolioProjectImageDTO.Remove(portofolioProjectImageDTO);
-            await _context.SaveChangesAsync();
+    //        _context.PortofolioProjectImageDTO.Remove(portofolioProjectImageDTO);
+    //        await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+    //        return NoContent();
+    //    }
 
     }
 }
