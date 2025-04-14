b474fb9e0cd299f51c3c38a3a5283d91de2179a0
AliIsmail10 <ali.mohamed.ali31199@gmail.com>
2025-04-14 20:30:47 +0200

    ProjectSkillsController

---
+++ IRepositoryService/IProjectSkillRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Freelancing.IRepositoryService
+{
+    public interface IProjectSkillRepository
+    {
+        Task<List<ProjectSkill>> GetAllAsync();
+        Task<ProjectSkill?> GetByIdAsync(int id);
+        Task<ProjectSkill> CreateProjectSkill(ProjectSkill projectSkill);
+        Task<ProjectSkill?> UpdateAsync(ProjectSkill projectSkill);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int projectId, int skillId);
+        Task<bool> ProjectExistsAsync(int projectId);
+        Task<bool> SkillExistsAsync(int skillId);
+    }
+}
