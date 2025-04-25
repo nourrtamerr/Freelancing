789a3352fa15cd7cea78fcaefa1439b01051edb1
Malak Emad <malakemad799@gmail.com>
2025-04-25 18:53:09 +0300

    Projects Controller

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Freelancing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+    }
+}
