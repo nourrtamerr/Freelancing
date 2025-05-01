1f14ce4c118ab9607fad41f13bf9e4913e4bda69
AliIsmail10 <ali.mohamed.ali31199@gmail.com>
2025-05-01 20:45:05 +0300

    image

--- DTOs/ChatDto.cs
+++ DTOs/ChatDto.cs
@@ -10,6 +10,8 @@ namespace Freelancing.DTOs
         public string SenderName { get; set; }
         public string ReceiverId { get; set; }
         public string ReceiverName { get; set; }
+
+
         public string Message { get; set; }
         public string ImageUrl { get; set; } 
         public DateTime SentAt { get; set; }
